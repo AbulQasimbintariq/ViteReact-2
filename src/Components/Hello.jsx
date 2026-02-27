8c66a43878c1e88ce4f3320e68d35b97178ed292
AbulQasimbintariq <www.qasimbhalli6547@gmail.com>
2026-02-27 23:12:57 +0500

    Add Twind UI

---
+++ src/Components/Hello.jsx
@@ -0,0 +1,18 @@
+export default function Hello() {
+    return (
+        <div class="bg-yellow-100 flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl">
+  <div>
+    <img class="size-48 shadow-xl rounded-md" alt="" src="/image.png" />
+  </div>
+  <div class="flex items-center md:items-start flex-col gap-1">
+    <span class="text-2xl font-medium">Class Warfare</span>
+    <span class="font-medium text-sky-500">The Anti-Patterns</span>
+    <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+      <span>No. 4</span>
+      <span>·</span>
+      <span>2025</span>
+    </span>
+  </div>
+</div>
+    )
+}
